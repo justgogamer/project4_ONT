7f8c7e605b2fba520f14e53d42ea32be157c4e7d
kaaskanaal <nicolekomen2001@gmail.com>
2020-06-07 21:03:08 +0200

    javascript
    javascript toegevoegd

---
+++ eenvoudrekenen/js/reeksen.js
@@ -0,0 +1,39 @@
+document.getElementById("fibBtn").addEventListener("click", function fibonacci(){
+    var num1 = 0;
+    var num2 = 1;
+    var input = document.getElementById("fibIn").value;
+    var ans = num1 + num2;
+    var fibField = document.getElementById("fibField");
+    fibField.innerHTML = 0;
+
+    while (ans < input){
+        fibField.innerHTML += " - " + ans;
+        ans = num1 + num2;
+        num1 = num2;
+        num2 = ans;
+    }
+}) ;
+
+var priBtn = document.getElementById("priBtn");
+
+priBtn.addEventListener("click", function display() {
+    var priVal = document.getElementById("priIn").value;
+    var uNum = [];
+    var num = 2;
+    for ( num = 2; num < priVal; num++ ) {
+        if ( priCheck(num) ) {
+            uNum.push(num);
+        }
+    }
+    document.getElementById("priField").innerHTML = uNum.toString(); // use arr result on your own
+})
+var priVal = document.getElementById("priIn").value;
+
+function priCheck(num) {
+    for ( var count = 2; count < num; count++ ) {
+        if ( num % count === 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
