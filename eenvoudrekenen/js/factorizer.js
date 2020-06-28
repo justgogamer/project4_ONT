f14a7be62b6f2695238619ebec80ba27cc7225f1
kaaskanaal <nicolekomen2001@gmail.com>
2020-06-28 21:00:52 +0200

    complete

--- eenvoudrekenen/js/factorizer.js
+++ eenvoudrekenen/js/factorizer.js
@@ -1,19 +1,74 @@
-var primeFactorization = function primeFactorization(number, result) {
-    var result = (result || []);
-    var root = Math.sqrt(number);
-    var x = 2;
-  
-    if (number % x) {
-      x = 3;
-  
-      while ((number % x) && ((x = (x + 2)) < root)) {}
+var getalInvoer, resultaat, arPrime = [], arFactor = [];
+
+function PrimeArray() {
+  for(var i = 2; i <= 1000; i++) {
+    var IsPrime = true;
+    for (var x = 2; x < i; x++) {
+      if (i % x == 0) {
+        IsPrime = false;
+      }
     }
-  
-    x = (x <= root) ? x : number;
-  
-    result.push(x);
-  
-    return (x === number) ? result : primeFactorization((number / x), result);
-  };
-  
-  Math.primeFactorization = primeFactorization;
+    if (IsPrime == true) {
+      arPrime.push(i);
+    }
+  }
+}
+
+function factorize() {
+  resultaat = "";
+  document.getElementById("answer").innerHTML = "";
+  arFactor = [];
+  getalInvoer = document.getElementById("factorizer").value;
+
+  for (var i = 0; i <= arPrime.length; i++) {
+    if (getalInvoer % arPrime[i] == 0) {
+      arFactor.push(arPrime[i]);
+      getalInvoer = getalInvoer / arPrime[i];
+      if (getalInvoer == 1) {
+        break;
+      } else {
+        i = -1;
+      }
+    }
+  }
+
+  for (var i = 0; i < arFactor.length; i++) {
+    resultaat += arFactor[i] + " * ";
+  }
+
+  resultaat = resultaat.substr(0, (resultaat.length - 3));
+  document.getElementById("answer").innerHTML = resultaat;    
+
+}
+
+
+var arHex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+var arOrigin = [];
+var gtOrigin, gtDestination, midResult, endResult;
+
+function convert() {
+  var nbOrigin = document.getElementById("ipGetal").value;
+
+  arOrigin = nbOrigin.split(' ')
+  arOrigin.reverse();
+
+  midResult = 0;
+  endResult = 0;
+
+  midResult = midStation();
+
+  document.getElementById("answer").innerHTML =
+  document.getElementById("ipGetal").value + "<sub>16</sub> = " +
+  midResult + "<sub>10</sub>";
+}
+
+function midStation() {
+  for (var i = 0; i < arOrigin.length; i++) {
+    midResult += (hexValue(arOrigin[i].toString().toUpperCase()) * Math.pow(16, i));
+  }
+  return midResult;
+}
+
+function hexValue(val) {
+  return arHex.indexOf(val);
+}
